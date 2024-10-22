0075614005a2128d3e01f65efd4c857eec3a7460
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-10-22 23:01:52 +0530

    day19

--- javascript/spread.js
+++ javascript/spread.js
@@ -1,7 +1,36 @@
 let arr = [1,2,4,3,11,22,33,23,13,123];
 
-console.log(...arr);
-console.log(arr);
+// console.log(...arr);
+// console.log(arr);
 
-console.log(Math.min(...arr));
-console.log(Math.max(...arr));
+// console.log(Math.min(...arr));
+// console.log(Math.max(...arr));
+
+
+
+
+//spread with string , array literals
+let even = [2,4,6,8];
+let odd = [1,3,5,7];
+
+let newArr = [...even , ...odd];
+// console.log(newArr);
+
+let str = "pratiksha"
+// console.log([...str]);
+
+let obj = {
+    email:"pk@gmail.com",
+    name:"pratiksha p"
+
+}
+
+let newObj = {
+    ...obj ,
+    age:90,
+    id:900,
+    place:"Karnataka"
+}
+
+console.log(newObj)
+
