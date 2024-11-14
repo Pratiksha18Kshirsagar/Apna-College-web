cb96f75d4bc8b546a16cfcd5140a0ea9e44fea0e
Pratiksha18Kshirsagar <149565116+Pratiksha18Kshirsagar@users.noreply.github.com>
2024-11-14 08:26:56 +0530

    day27

--- simon-game/script.js
+++ simon-game/script.js
@@ -24,6 +24,7 @@ function btnFlash(btn) {
 }
 
 function levelUp() {
+    userSeq = [];
     level++;
     h2.innerText = `level ${level}`;
     let randomIdx = Math.floor(Math.random() * 3);
@@ -35,8 +36,22 @@ function levelUp() {
 
 }
 
-function checkans(){
-
+function checkans(idx) {
+    
+    if (userSeq[idx] == gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp , 1000);
+        }
+        
+    }
+    else {
+        h2.innerHTML = `GameOver !! Your score was <b>${level}</b> <br>  Please press any key .`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(() => {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
 }
 
 function btnPress() {
@@ -46,11 +61,18 @@ function btnPress() {
     btnFlash(btn);
     userColor = btn.getAttribute("id");
     userSeq.push(userColor);
-    checkans();
+    checkans(userSeq.length-1);
 }
 
 let allButtons = document.querySelectorAll(".box");
 for (let button of allButtons) {
     button.addEventListener("click", btnPress);
 
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
