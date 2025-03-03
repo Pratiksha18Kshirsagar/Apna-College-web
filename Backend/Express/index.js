cc28c2d4a73c4adba01e10455d222c0e1a972045
Pratiksha18Kshirsagar <kshirsagarpratiksha1820@gmail.com>
2025-03-03 21:29:30 +0530

    day41

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -17,13 +17,27 @@ app.get("/home",(req,res)=>{
 res.send("We are in home page");
 })
 
-app.get("/search",(req,res)=>{
-    res.send("We are in search page");
-    })
- app.get("/apple",(req,res)=>{
-    res.send("We are in apple page");
-     })
+// app.get("/search",(req,res)=>{
+//     res.send("We are in search page");
+//     })
+//  app.get("/apple",(req,res)=>{
+//     res.send("We are in apple page");
+//      })
 
-    app.get("*",(req,res)=>{
-        res.send("the path doest not exist!!!");
-        })
+//  app.get("*",(req,res)=>{
+//     res.send("the path doest not exist!!!");
+//     })
+
+app.get("/ig/:userName/:id" , (req,res) =>{
+    let  {userName , id} = req.params ;
+    console.log(userName , id);
+    res.send(`welcome to ${userName} ${id} page`)
+})
+
+app.get("/Search" , (req,res)=>{
+    let {q} = req.query;
+    if(!q){
+       res.send("No search query");
+    }
+    res.send(`These are the results for : ${q}`);
+});
