ce23d35b9b213010deb58d50b982f04052dd7aaf
Pratiksha18Kshirsagar <kshirsagarpratiksha1820@gmail.com>
2025-03-04 21:59:41 +0530

    day42

--- Backend/ejsDir/index.js
+++ Backend/ejsDir/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const app =express();
+const path = require("path");
 
 const port = 8080;
 
 app.listen(port , ()=>{
     console.log(`App is listening on port ${port}.`);
+})
+
+app.set("view Engine" , "ejs");
+app.set("views" , path.join(__dirname , "/views"));
+
+app.get("/" , (req,res)=>{
+    res.render("home.ejs");
+})
+
+app.get("/rollDice" , (req,res)=>{
+    let diceval = Math.floor(Math.random()*6)+1;
+    res.render("rollDice.ejs" , {diceval});
+})
+
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get("/ig/:username" , (req,res)=>{
+    const instaData = require("./data.json");
+    console.log(instaData.cats);
+    let {username} = req.params;
+    let data = instaData[username];
+    console.log(data);
+    res.render("instagram.ejs", { data } );
 })
